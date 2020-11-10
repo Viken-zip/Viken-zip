90b627a4af7427dd11c8055125fdc3fb96874c12
Viken-zip <assiviikman@gmail.com>
2020-11-10 09:58:58 +0200

    yes

---
+++ reeeee/C++/script.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()                             
+{
+    int slices = 5;
+    cout << "how mnay slices do yo watn fatty?";
+    cin >> slices;
+    cout << "you have " << slices << " slices of pizza. "<< endl;
+}
