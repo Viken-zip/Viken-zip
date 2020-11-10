2cb0bddf4dee04cf6dac4437104323fe23ecb492
Viken-zip <assiviikman@gmail.com>
2020-11-10 15:18:44 +0200

    oh yes

--- reeeee/shopping_site/script.js
+++ reeeee/shopping_site/script.js
@@ -1,3 +1,12 @@
+var box = document.getElementById('sidebar');
+var contentWidth = box.clientWidth;
+
 document.getElementById('btn').addEventListener('click', function () {
-    document.querySelector('.box').classList.add('grow');
+    if(contentWidth > 250){
+    document.querySelector('.sidebar').classList.remove('yep');
+    document.querySelector('.sidebar').classList.add('kek');
+    }else{
+    document.querySelector('.sidebar').classList.remove('kek');
+    document.querySelector('.sidebar').classList.add('yep');
+    }
   });
